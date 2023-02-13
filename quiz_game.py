9e1d8ed308d089a3ba36c66fa22c6f246bb0f533
Khavushka <khavushka@gmail.com>
2023-02-13 15:06:23 +0100

    just begin

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,14 @@
-print("Hello")
+print("Welcome to my quiz-game!");
+
+playing = input('Do you want to play? ')
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct!")
+else: 
+    print("Incorrect")
