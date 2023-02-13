0b8eacb52d68d440826678b35346bedc5a359caa
Khavushka <khavushka@gmail.com>
2023-02-13 15:21:11 +0100

    adding procent

--- quiz_game.py
+++ quiz_game.py
@@ -2,13 +2,32 @@ print("Welcome to my quiz-game!");
 
 playing = input('Do you want to play? ')
 
-if playing != "yes":
+if playing.upper() != "YES":
     quit()
 
 print("Okay! Let's play")
+score = 0
 
 answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1
 else: 
     print("Incorrect")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score/3)*100) + " %")
